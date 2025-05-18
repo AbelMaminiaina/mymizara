37ec98d31d29a3cc858a47e9ce5d5ab4e4ae161a
YRabetsimialona <139714687+YRabetsimialona@users.noreply.github.com>
2025-05-18 02:16:31 +0200

    modify package and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import LandingPage  from './Layout/LandingPage';
 
 
